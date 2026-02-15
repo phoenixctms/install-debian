f50c16978f1e41e10bac87d9cf5f75d4d8fded2c
Rene Krenn <rkrenn@phoenixctms.org>
2026-02-15 16:00:49 +0100

    tomcat9 -> tomcat10 directory

--- dbtool.sh
+++ dbtool.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 CTSMS_PROPERTIES=${CTSMS_PROPERTIES:-/ctsms/properties}
 CTSMS_JAVA=${CTSMS_JAVA:-/ctsms/java}
-CATALINA_BASE=${CATALINA_BASE:-/var/lib/tomcat9}
+CATALINA_BASE=${CATALINA_BASE:-/var/lib/tomcat10}
 java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dfile.encoding=Cp1252 -Djava.awt.headless=true -Xms2048m -Xmx4096m -Xss256k -XX:+UseParallelGC -XX:MaxGCPauseMillis=1500 -XX:GCTimeRatio=9 -XX:+CMSClassUnloadingEnabled -XX:ReservedCodeCacheSize=256m -classpath $CATALINA_BASE/webapps/ROOT/WEB-INF/lib/ctsms-core-1.9.0.jar:$CATALINA_BASE/webapps/ROOT/WEB-INF/lib/* org.phoenixctms.ctsms.executable.DBTool $*
