04ecc73579bdeb289306f78aa1dbe6af9ab23a92
Rene Krenn <rkrenn@phoenixctms.org>
2026-02-24 04:38:50 +0100

    removed hardcoded version from dbtool.sh

--- dbtool.sh
+++ dbtool.sh
@@ -2,4 +2,6 @@
 CTSMS_PROPERTIES=${CTSMS_PROPERTIES:-/ctsms/properties}
 CTSMS_JAVA=${CTSMS_JAVA:-/ctsms/java}
 CATALINA_BASE=${CATALINA_BASE:-/var/lib/tomcat10}
-java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dfile.encoding=Cp1252 -Djava.awt.headless=true --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED -Xms2048m -Xmx4096m -Xss256k -XX:+UseParallelGC -XX:MaxGCPauseMillis=1500 -XX:GCTimeRatio=9 -XX:ReservedCodeCacheSize=256m -classpath $CATALINA_BASE/webapps/ROOT/WEB-INF/lib/ctsms-core-1.9.0.jar:$CATALINA_BASE/webapps/ROOT/WEB-INF/lib/* org.phoenixctms.ctsms.executable.DBTool $*
+CORE_JAR=$(ls $CATALINA_BASE/webapps/ROOT/WEB-INF/lib/ctsms-core-*.jar)
+LIB_JARS=$(ls $CATALINA_BASE/webapps/ROOT/WEB-INF/lib/*.jar | tr '\n' ':')
+java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dfile.encoding=Cp1252 -Djava.awt.headless=true --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED -Xms2048m -Xmx4096m -Xss256k -XX:+UseParallelGC -XX:MaxGCPauseMillis=1500 -XX:GCTimeRatio=9 -XX:ReservedCodeCacheSize=256m -classpath $CORE_JAR:$LIB_JARS org.phoenixctms.ctsms.executable.DBTool $*
