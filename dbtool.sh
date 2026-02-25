5d343cf00ab427eb9b2be84f8c61b02f920c55da
Rene Krenn <rkrenn@phoenixctms.org>
2026-02-25 18:16:47 +0100

    fix dbtool.sh to use either tomcat9 or tomcat10

--- dbtool.sh
+++ dbtool.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 CTSMS_PROPERTIES=${CTSMS_PROPERTIES:-/ctsms/properties}
 CTSMS_JAVA=${CTSMS_JAVA:-/ctsms/java}
-CATALINA_BASE=${CATALINA_BASE:-/var/lib/tomcat10}
+if [ -d "/var/lib/tomcat10" ]; then
+    CATALINA_BASE=${CATALINA_BASE:-/var/lib/tomcat10}
+else
+    CATALINA_BASE=${CATALINA_BASE:-/var/lib/tomcat9}
+fi
 CORE_JAR=$(ls $CATALINA_BASE/webapps/ROOT/WEB-INF/lib/ctsms-core-*.jar)
 LIB_JARS=$(ls $CATALINA_BASE/webapps/ROOT/WEB-INF/lib/*.jar | tr '\n' ':')
 java -DCTSMS_PROPERTIES="$CTSMS_PROPERTIES" -DCTSMS_JAVA="$CTSMS_JAVA" -Dfile.encoding=Cp1252 -Djava.awt.headless=true --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED -Xms2048m -Xmx4096m -Xss256k -XX:+UseParallelGC -XX:MaxGCPauseMillis=1500 -XX:GCTimeRatio=9 -XX:ReservedCodeCacheSize=256m -classpath $CORE_JAR:$LIB_JARS org.phoenixctms.ctsms.executable.DBTool $*
